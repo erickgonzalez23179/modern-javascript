12ba69af898059853a59fa1fe6d181f421e4d990
Erick <egonzalez23179@columbushs.com>
2021-01-26 12:22:15 -0500

    finished

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -1,20 +1,17 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
-const form = document.querySelector('.quiz-form');
+const form = document.querySelector(`.quiz-form`)
 
-form.addEventListener('submit', e => {
+form.addEventListener(`submit`, e => {
   e.preventDefault();
 
   let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
 
-  // check the answers
+  // check answers
   userAnswers.forEach((answer, index) => {
-    if (answer === correctAnswers[index]){
+    if(answer === correctAnswers[index]){
       score += 25;
     }
-  });
-
-  // log the score to console
-  console.log(score);
-
-});
+  })
+  console.log(score)
+})
